0d24bb06c3b122fdf861b590110bf8d78b49926d
Aaron Wilder <element1028@gmaill.com>
2018-07-13 14:54:25 -0700

    Policies page (issue #19)
    add policies page to footer

---
+++ src/Styles/PolicyStyles.js
@@ -0,0 +1,114 @@
+import styled from 'styled-components';
+import {FadeIn, Border, BoxShadow,
+        MainFont1, MainFont2, Blue,
+        TextShadow, FlexCol, Animation,
+        RedBorderBottom } from './MainStyles';
+
+export const MainArticle = styled.article`
+  border: ${Border};
+  box-shadow: ${BoxShadow};
+  animation-name: ${FadeIn};
+  ${Animation}
+  margin-top: 15px;
+  @media(min-width: 1300px) {
+    margin-left: 85px;
+    margin-right: 85px;
+  }
+`;
+
+export const WarranyName = styled.h4`
+  font-family: ${MainFont1};
+  border-bottom: ${RedBorderBottom};
+  text-align: center;
+  margin: 0 auto;
+  font-size: 1rem;
+  letter-spacing: .09rem;
+  @media(min-width: 768px) { font-size: 1.5rem; letter-spacing: .12rem;}
+  @media(min-width: 1022px) { font-size: 1.7rem; letter-spacing: .20rem; }
+`;
+
+export const WarranyNumber = styled.a`
+  color: ${Blue};
+  font-family: ${MainFont2};
+  margin-top: 10px;
+  margin-bottom: 20px;
+  text-align: center;
+  font-size: 1.2rem;
+  letter-spacing: .12rem;
+  &:hover {
+    color: #eb1c24;
+  }
+  @media(min-width: 768px) { 
+    font-size: 1.8rem;
+    letter-spacing: .15rem;
+    }
+`;
+
+export const WarrantyTopper = styled.h3`
+  font-family: ${MainFont1};
+  text-shadow: ${TextShadow};
+  background-color: ${Blue};
+  padding: 20px; 
+  line-height: 1.2rem;
+  letter-spacing: .1rem;
+  text-align: center;
+  color: white; 
+  @media(min-width:768px) {
+    line-height: 1.8rem;
+    letter-spacing: .18rem;
+    font-size: 1.4rem;
+    padding-left: 100px;
+    padding-right: 100px;
+  }
+  @media(min-width: 1024px) { 
+    padding-left: 140px;
+    padding-right: 140px;
+    font-size: 1.6rem; 
+    line-height: 2.4rem;
+    letter-spacing: .22rem;}
+`
+
+export const WarrantyWrapper = FlexCol.extend`
+  justify-content: center;
+`;
+
+export const P = styled.p`
+  font-family: ${MainFont2};
+  text-indent: 25px;
+  line-height: 1.35rem;
+  padding-right: 8px;
+  padding-left: 8px;
+  margin-top: 10px;
+  @media(min-width: 768px) {
+    font-size: 1.5rem;
+    line-height: 1.8rem;
+    padding-right: 17px;
+    padding-left: 17px;
+  }
+  @media(min-width: 1024px) { 
+    font-size: 1.6rem;
+    line-height: 2.1rem;
+    margin-bottom: 10px;
+    padding-right: 25px;
+    padding-left: 25px;
+    padding-bottom: 15px;
+    padding-top: 15px;
+    }
+`;
+
+export const Heading = styled.h3`
+  font-family: ${MainFont2};
+  border-bottom: ${RedBorderBottom};
+  padding-bottom: 5px;
+  padding-left: 10px;
+  margin-bottom: 8px;
+  @media(min-width: 768px) {
+    font-size: 1.7rem;
+    padding-bottom: 8px;
+  }
+  @media(min-width: 1024px) {
+    padding-top: 10px;
+    margin-bottom: 20px;
+    font-size: 1.8rem;
+  }
+`; 
---
+++ src/pages/policies.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { MainArticle, P, Heading } from '../Styles/PolicyStyles';
+// import Helmet from 'react-helmet';
+import { H2 } from '../Styles/MainStyles';
+import Layout from '../components/layout';
+
+export default () => (
+  <Layout>
+    <MainArticle>
+      {/* <Helmet>
+        <title>ESC: Terms and Policies</title>
+        <meta name="description" content="Terms and Policies for E.S.C Mattress Center website"/>        
+        <meta property="og:type"   content="website" />
+        <meta property="og:site_name" content="E.S.C. Mattress Center"/>
+        <meta property="og:url" content="https://www.escmattresscenter.com/"/>
+        <meta property="og:image:secure_url" content="https://www.escmattresscenter.com/static/media/logo.952ae51d.png"/> 
+        <meta property="og:image:width" content="1200"/>
+        <meta property="og:image:height" content="627"/>
+        <meta property="og:image:alt" content="E.S.C Mattress Center's logo of a panda"/>
+        <meta property="og:title" content="E.S.C. Mattress Center | Terms and Policies"/>
+        <meta property="og:description" content="A licensed mattress retailer for Sealy, Stearns and Foster, and Tempur-Pedic offering almost fifty mattresses with prices to fit every budget.  ESC Mattress Center is a locally owned company in Everett WA with more than twenty years of mattress industry experience between our non-commissioned staff."/>
+      </Helmet> */}
+      <header>
+        <H2>Terms and Policies</H2>
+      </header>
+      <Heading>90 Day Comfort Guarantee</Heading>
+      <P>
+        It will take anywhere from 30 to 90 nights for your new mattress to break in and for your body
+        to adjust to the new support and comfort. Because of this, we ask that you sleep on your new mattress
+        for a minimum of 30 nights to allow for adjustment. However, if you are not comfortable
+        within 90 days of your original mattress’s delivery date, you may exchange or return your mattress one time in this period.
+      </P>
+      <P>
+        Delivery and set up fees are non-refundable. We do not charge restocking fees, only a redelivery fee of $79.99.
+        E.S.C. Mattress Center is unable to exchange or refund any mattress that is stained, soiled,
+        or in any way damaged by you under this guarantee. Do not remove the Law Label from your mattress
+        or foundation, doing so will void your 90 Day Comfort Guarantee. Please be aware that our Comfort
+        Guarantee does not cover clearance merchandise, floor models, warranty exchanges, furniture,
+        adjustable bases, pillows, mattress protectors, or other accessories. See associate for more details.
+      </P>
+      <Heading>Low Price Guarantee</Heading>
+      <P>
+        E.S.C. Mattress Center guarantees to meet, or beat, any other licensed retailer’s price on the same
+        or comparable mattress set advertised for less than your invoiced price within 90 days of purchase.
+        Bring in the advertisement and we will refund you the difference within 14 days. Note: Products for 
+        sale on auction sites, such as eBay, Craigslist, etc. are excluded. Applies to new product only.
+      </P>
+      <Heading>Delivery</Heading>
+      <P>
+        We use a professional, licensed, outside delivery service for our deliveries. E.S.C. Mattress Center
+        is not responsible for any loss or damage caused during delivery. Please make sure there is a clear 
+        path from the door to the area you would like your mattress set placed. Removal is limited to number 
+        of pieces being delivered and is limited to mattresses and foundations. Frames, furniture, waterbeds,
+        etc. cannot be removed.
+      </P>
+      <P>
+        If you choose to pick up your product, you should inspect your items carefully
+        prior to leaving our store. We recommend the use of a covered truck for transporting mattress sets. 
+        If requested, we will help you load and secure the merchandise into your vehicle, at your sole risk. 
+        E.S.C. Mattress Center cannot be responsible for any damaged caused by our assistance. Any claim for 
+        loss or damage due to the transportation of merchandise will be the sole responsibility of the customer.
+      </P>
+    </MainArticle>
+  </Layout>
+  )
+
+
