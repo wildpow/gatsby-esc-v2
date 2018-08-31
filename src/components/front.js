b12fde67d8aa228fd59cb333b4e7036e2d6c07e5
wildpow <element1028@gmail.com>
2018-08-31 16:34:26 -0700

    Getting front page info from GraphCMS now

--- src/components/front.js
+++ src/components/front.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Headline, Wrapper, P } from '../Styles/HomeStyles';
-
+import { StaticQuery, graphql } from "gatsby";
 
 const Front = () => {
   return (
     <Wrapper>
-      <Headline>The Real Deal</Headline>
-      <P>
-      The Real Deal is going on now, save up to $1000 on popular mattress sets! Save $550 on Tempur-Pedic Elite, Luxe, and Breeze models or upgrade to an Adjustable Base at no additional charge. Upgrade to the highest rated luxury innerspring and save up to $1000 on Stearns & Foster Adjustable mattress sets. Save up to $200 on top-rated Sealy Hybrid and Conform mattress models. The biggest mattress sale of the year is going on now with The Real Deal now through September 10th, so stop by today and save!
-      </P>
+      <StaticQuery
+        query={graphql`
+          query front {
+            front {
+              id
+              title
+              textBlock
+            }
+          }
+        `}
+        render={data => (
+          <>
+          <Headline>{data.front.title}</Headline>
+            <P>
+              {data.front.textBlock}
+            </P>
+          </>
+        )}
+      />
       <Headline red>“Sleep Like the Experts Do!”</Headline>
     </Wrapper>
   )
