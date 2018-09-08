a24367ffc1e52927606b52655aadb208fb2bcefe
wildpow <element1028@gmail.com>
2018-09-08 14:18:15 -0700

    getting data in single adjustable base

--- src/templates/base.js
+++ src/templates/base.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { graphql } from "gatsby"
 
 const Base = (props) => {
   return (
@@ -11,3 +12,11 @@ const Base = (props) => {
 }
 
 export default Base;
+
+export const query = graphql`
+  query SingleAjustableQuery($uri: String!) {
+    adjBasese(uri: { eq: $uri}) {
+      uri
+    }
+  }
+`
