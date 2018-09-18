b5254d917825b704af7a7415a99410ca6ba49101
wildpow <element1028@gmail.com>
2018-09-18 08:21:59 -0700

    Dropped google analytics plug-in to see if that was the cause of speed test going down

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,13 +25,6 @@ module.exports = {
       display: `minimal-ui`,
       icon: `src/images/logo.png`, // This path is relative to the root of the site.
     },
-   
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: `UA-112381210-1`,
-      },
     },
     `gatsby-plugin-offline`,`gatsby-plugin-netlify`],
 }
